e0e9464d0f2fba4b8222dbedb7d2a8727c51fe42
developSecurity <kimseungwon9297@gmail.com>
2018-08-29 01:22:35 +0900

    css 수정 파일 설계

---
+++ js/register.js
@@ -0,0 +1,8 @@
+<script>
+  var join = document.getElementById('id_join');
+  window.onclick = function(event) {
+      if(event.target == join) {
+          join.style.display = "none";
+      }
+  }
+</script>
